54e5166ca3c0ed1a2f3e6b17c55faea8dba4aa5b
jjsammii <85793576+jjsammii@users.noreply.github.com>
2023-03-14 15:22:25 -0500

    Update 3_📈_Predictions.py

--- pages/3_📈_Predictions.py
+++ pages/3_📈_Predictions.py
@@ -30,7 +30,8 @@ st.write("---")
 
 @st.cache
 def get_data():
-    df_pred = pd.read_csv('data/predictions.csv')
+    AZ_BUCKET_URL = "https://formula1fujpoc1.blob.core.windows.net/farmgate"
+    df_pred = pd.read_csv(AZ_BUCKET_URL + "/predictions.csv")
     df_pred["month1"] = pd.to_datetime(df_pred['price_date']).dt.month
     df_pred["month"] = pd.to_datetime(df_pred['price_date']).dt.strftime('%B')
     df_pred["year"] = pd.to_datetime(df_pred['price_date']).dt.strftime('%Y')
